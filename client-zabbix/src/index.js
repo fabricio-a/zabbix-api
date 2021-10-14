eadff9215893c6d223f445e912ff1ea352ac3285
= <=>
2021-10-14 15:30:32 -0300

    commit inicial

--- client-zabbix/src/index.js
+++ client-zabbix/src/index.js
@@ -9,3 +9,35 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+/*
+            <FormControl sx={{ m:1, minWidth: 300, mt:3 }}>
+                <Select
+                    multiple
+                    displayEmpty
+                    value={selectedHosts}
+                    onChange={(e) => {}}
+                    input={<OutlinedInput />}
+                    renderValue={(selected) => {
+                        if (selected.length === 0) {
+                            return <em>Placeholder</em>;
+                        }
+            
+                        return selected.join(', ');
+                    }}
+                    inputProps={{ 'aria-label': 'Without label' }}
+                    >
+                        <MenuItem disabled value="">
+                            <em>Hosts</em>
+                        </MenuItem>
+
+                        {allHosts.map((host) => (
+                            <MenuItem
+                                key={host.hostid}
+                                value={host.hostid}
+                            >
+                                {host.host}
+                            </MenuItem>
+                    ))}
+                </Select>
+            </FormControl> */
--- client-zabbix/src/screens/MainScreen.js
+++ client-zabbix/src/screens/MainScreen.js
@@ -1,16 +1,31 @@
 import * as React from 'react'
 import './mainScreen.css'
-import InputLabel from '@mui/material/InputLabel';
+import { TextField } from '@mui/material';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { OutlinedInput } from '@mui/material';
 
 export default function MainScreen() {
-    const [hosts, setHosts] = React.useState([])
+    const [allHosts, setHosts] = React.useState([])
+    const [formState, setFormState] = React.useState({
+        hosts: []
+    })
+
+    React.useEffect(() => {
+        fetch('http://localhost:4444/generate-report/hosts')
+            .then(res => res.json())
+            .then(hosts => {
+                setHosts(hosts)
+
+                console.log(hosts)
+            })
+    }, [])
 
     return (
-        <div>
+        <div className='mainScreen'>
             <h2>Report Generator Roost</h2>
+
         </div>
     )
 }
